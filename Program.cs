7c0c9f194d619eca4896b292da0ac15e930a527a
Mojtaba Goodarzi <mojbaba@wallit.eu>
2023-04-27 15:56:10 +0330

    generate core blockchain extended pub/priv keys

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Xcb.Net.Extensions;
+using Xcb.Net.HDWallet;
+
+var masterExtendedPrivateKey = ExtendedPrivateKey.GenerateRandomMaster();
+
+var keyBytes = (byte[])masterExtendedPrivateKey;
+
+System.Console.WriteLine("Master Extended Private Key: ");
+
+System.Console.WriteLine(keyBytes.ToHex());
+
+
+var masterExtendedPublicKey = masterExtendedPrivateKey.ToExtendedPublicKey();
+
+System.Console.WriteLine("Master Extended Public Key: ");
+
+System.Console.WriteLine(((byte[])masterExtendedPublicKey).ToHex());
